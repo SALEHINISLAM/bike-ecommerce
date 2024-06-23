1668973ab88d390e910f8c11a88d17ea7b27b2b9
SALEHIN <msionlinekingdom@gmail.com>
2024-06-23 16:13:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'media',
+    // content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundColor:{
+          default:'#fff',
+        },
+      },
+    },
+    plugins: [],
+  }
